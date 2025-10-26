8b36adc1a56a58bd18a1ca8a968097e24b32532b
Subhankar <maitisubhankar55@gmail.com>
2025-10-26 16:26:39 +0530

    update all

--- Src/utils/index.js
+++ Src/utils/index.js
@@ -1,24 +1,8 @@
 import dotenv from "dotenv";
 import connectDB from "../db/index.js";
 dotenv.config({
-    path: './env'
+    path: './.env'
 });
 
 connectDB();
-// import dotenv from "dotenv";
-// import express from "express";
-// dotenv.config();
 
-// const app = express();
-
-// (async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-//         app.listen(process.env.PORT, () => {
-//             console.log(`App is listening on port ${process.env.PORT}`);
-//         });
-//     } catch (error) {
-//         console.error("ERROR: ", error);
-//         throw error;
-//     }
-// })();
